efbc1210ad85ddd276c564c4b4d5e2b80f94e63e
Kelvin PM <78834838+KelvinJ1@users.noreply.github.com>
2023-09-14 10:42:30 -0500

    Add files via upload

--- Client.py
+++ Client.py
@@ -2,61 +2,78 @@ import socket
 from dotenv import load_dotenv
 import os
 import re
+import threading
+import time
 
 load_dotenv()
 
 #Accesing venv variables
-HOST = os.environ["HOST"] # Change to the IP address of your Node.js server if it's on a different machine
+HOST = os.environ["HOST"] 
 PORT = int(os.environ["PORT"])
+
+# Time-out for sync
+response_received = threading.Event()
+
+# Capturing servers responses
+def receive_messages():
+    while (True):
+        try:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            print(f'Mensaje del servidor: {data.decode()}')
+            #Flagging a response from the server
+            response_received.set()
+            
+        except Exception as e:
+            print(f'Error al recibir datos del servidor: {e}')
+            break
 
 #try-except 
 try:
-    # Create a socket object
+    # new socket obj
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Connect to the server
     client_socket.connect((HOST, PORT))
-    print(f'Connected to {HOST}:{PORT}')
+    print(f'Conectado a {HOST}:{PORT}')
 
+    # Starting thread for servers responses
+    receive_thread = threading.Thread(target=receive_messages)
+    receive_thread.daemon = True
+    receive_thread.start()
 
-    
-    loop_status=1
-    while (loop_status==1):
+    while (True):
         
-        format=r'^[0-9A-Fa-f]{4}[01D]$'
-        device_number=hex(int(input("\nIngrese el código del equipo en decimal: ")))
-        device_status=input("¿El equipo está conectado? (1 para conectado, 0 para desconectado, D para eliminar): ")
-        msg=device_number[2:].zfill(4)+device_status
+        format=r'^<\[[0-9A-Fa-f]{4}[01D]]\>$' #format that the msg must follow
+
+        device_number=hex(int(input("\nIngrese el código del equipo en decimal: "))) #device number input and conversion to hex
+        device_status=input("¿El equipo está conectado? (1 para conectado, 0 para desconectado, D para eliminar): ") #requestion device status
+        msg=f"<[{device_number[2:].zfill(4)}{device_status}]>" #fullfilling msg 
+    
+        #Validating that the outgoing msg has the required format
         if re.match(format, msg):
-            print("\nMensaje a ser enviado al servidor: "+msg)
+            print("\nMensaje enviado: "+msg)
             print("\n**********************************************")
             try:
                 # Send data to the server 
                 message = msg
                 client_socket.sendall(message.encode())
-            except Exception as e:
-                    print(f'Error: {e}')
 
+                #awaiting for servers response
+                response_received.wait()
+                response_received.clear()
+
+            except Exception as e:
+                    print(f'Hubo un error al enviar datos al servidor, por favor verifique código fuente: {e}')
         else:
             print("Los datos ingresados no cumplen con el formato.")
-                        
-            
-
 
     
-
-    # Receive data from the server
-    data = client_socket.recv(1024)
-    print(f'Received from server: {data.decode()}')
-
+    
 except Exception as e:
-    print(f'Error: {e}')
+    print(f'Al parecer hubo un error en la conexión al servidor, por favor verificar: {e}')
+    client_socket.close()
 
-#loop status =1, will continue requesting data
-#loop status = 0, will drop the server.
-
-
-
-#client_socket.close()
 
 
--- Server.js
+++ Server.js
@@ -1,66 +1,84 @@
 const net = require('net');
 
+// creating server instance
 const server = net.createServer((socket) => {
     console.log('Cliente conectado');
 
 
-
+    // devices obj
     const devices = {}
-    const format =/^[0-9A-F0-9A-Fa-f]{4}[01D1]$/;   
+    // format to be followed by the msgs
+    const format =/^<\[([0-9A-F0-9A-Fa-f]{4}[01D1])\]>$/; 
+    
+    // if there is incoming data from the client prints the incoming msg
     socket.on('data', (data) => {
-        console.log(`\nReceived data: ${data}`);
-        console.log("*************************************************\n");
-        const incoming_code= data.toString().trim();
-        
-        if (format.test(incoming_code)) {
-            const device_number= parseInt(incoming_code.slice(0,4) ,16);
-            const device_status=incoming_code.slice(-1);
-
-            if(devices[device_number]){
-                 // Device already exists, update device_status
-                    
-                    if (device_status==="D") {
-                        delete devices[device_number];
-                        console.log(`\nEl dispositivo ${device_number} ha sido eliminado del registro.`)                        
-                    }
-                    else if (devices[device_number].status==="1" && device_status==="1") {
-                        console.log(`\nEl dispositivo ${device_number} ya se encuentra conectado (${device_status}).`);
-                    } 
-                    else if(devices[device_number].status==="0" && device_status==="0"){
-                            console.log(`\nEl dispositivo ${device_number} ya se encuentra desconectado (${device_status}).`);
-                    }
-                    else if(devices[device_number].status!==device_status){
-                        devices[device_number].status = device_status;
-                        console.log(`\nEl estado del dispositivo ${device_number} ha sido actualizado a (${device_status}).`);
-                    }
-                    
-                    
-            }else if(!devices[device_number] && device_status==="D"){
-                console.log("\nEste dispositivo no se puede eliminar ya que no existe.")
-
+        try {
+            console.log(`\nData proveniente del cliente: ${data}`);
+            console.log("*************************************************\n");
+            // forming a string with the incoming data and removing whitespaces
+            const incoming_code= data.toString().trim(); 
+            if (format.test(incoming_code)) {
+                const converted_code= incoming_code.replace(/[<>\[\]]/g, '');
+                const device_number= parseInt(converted_code.slice(0,4) ,16);
+                const device_status=converted_code.slice(-1);
+    
+                // if the incoming device number is already in the devices obj will execute an action based on the status
+                if(devices[device_number]){
+                        if (device_status==="D") {
+                            delete devices[device_number];
+                            console.log(`\nEl dispositivo (${device_number}) ha sido eliminado del registro.`);
+                            socket.write(`\nDispositivo (${device_number}) eliminado.`);
+                        }
+                        else if (devices[device_number].status==="1" && device_status==="1") {
+                            console.log(`\nEl dispositivo (${device_number}) ya se encuentra conectado (${device_status}).`);
+                            socket.write(`\nEl estado del dispositivo ya era conectado (1)`);
+                        } 
+                        else if(devices[device_number].status==="0" && device_status==="0"){
+                                console.log(`\nEl dispositivo (${device_number}) ya se encuentra desconectado (${device_status}).`);
+                                socket.write(`\nEl estado del dispositivo ya era desconectado(0)`);
+                        }
+                        else if(devices[device_number].status!==device_status){
+                            devices[device_number].status = device_status;
+                            console.log(`\nEl estado del dispositivo (${device_number}) ha sido actualizado a (${device_status}).`);
+                            socket.write(`\nEl estado del dispositivo (${device_number}) ha sido actualizado`);
+                        }
+                        
+                        
+                }else if(!devices[device_number] && device_status==="D"){
+                    console.log("\nEste dispositivo no se puede eliminar ya que no existe.")
+                    socket.write(`\nEste dispositivo no se puede eliminar ya que no existe.`);
+    
+                }
+                else{
+                    devices[device_number]={number:device_number, status: device_status}
+                    console.log("\nNuevo dispositivo añadido")
+                    socket.write(`\nDispositivo agregado al registro`);
+                } 
+                const deviceList = Object.keys(devices).map(deviceKey => `${devices[deviceKey].number}: ${devices[deviceKey].status}`).join(', ');
+                console.log(`\nLista de dispositivos en el registro: ${deviceList}`);  
+                
             }
-            else{
-                devices[device_number]={number:device_number, status: device_status}
-                console.log("\nNuevo dispositivo añadido")
-            } 
-            const deviceList = Object.keys(devices).map(deviceKey => `${devices[deviceKey].number}: ${devices[deviceKey].status}`).join(', ');
-            console.log(`\nLista de dispositivos en el registro: ${deviceList}`);  
             
+        } catch (error) {
+            console.error(`Error al tratar la data proveniente del cliente: ${error}`);            
         }
-        
-        // You can send a response to the client here if needed.
+
     });
 
+    // If client loses cnxn
     socket.on('end', () => {
         console.log('Cliente desconectado');
     });
 
+    //handling error if cnxn fails
     socket.on('error', (err) => {
-        console.error(`Error: ${err}`);
+        console.error(`Error en conexión: ${err}`);
     });
 });
 
-const PORT = 3000; // Change this to your desired port number
+// active on port:
+const PORT = 3000; 
+
 
 server.listen(PORT, () => {
     console.log(`\nServer escuchando en el puerto: ${PORT}`);
